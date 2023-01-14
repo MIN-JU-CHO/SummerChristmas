1af53372cab806b083eccc6b2b0be9e6e6af66f4
MIN-JU-CHO <ehdnf99@sookmyung.ac.kr>
2023-01-14 17:09:43 +0900

    BGMove, CameraMove

--- Assets/Scripts/BackgroundMove.cs
+++ Assets/Scripts/BackgroundMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Background : MonoBehaviour
+public class BackgroundMove : MonoBehaviour
 {
     public float speed = 0f;
 
