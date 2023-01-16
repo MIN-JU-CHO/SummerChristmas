d4f063ef445b09a193eff5fc62e32225913bcb09
MIN-JU-CHO <ehdnf99@sookmyung.ac.kr>
2023-01-16 21:55:56 +0900

    Replay 버튼

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -48,6 +49,11 @@ public class GameManager : MonoBehaviour
         //SetWidthBG(0f);
         // 엔딩 띄우기
         
+    }
+
+    public void ClickedReplay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     private void SetHeightBG(float speed)
